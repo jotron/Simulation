fd892652a4afe72715b4161c274db3c39bd63d6c
7gh-w <39625441+7gh-w@users.noreply.github.com>
2018-05-27 11:32:32 +0200

    Update Runge-Kutta 4..py

--- Runge-Kutta 4..py
+++ Runge-Kutta 4..py
@@ -27,4 +27,4 @@ while t <= 5:
         print("y(%0.1f)\t= %6.2f " % ( t, y))
 
         """define new values for y(t) and t to begin a new while loop"""
-        t, y = t + dt, y + dy( t, y, dt )
+        y, t = y + dy( t, y, dt ), t + dt
