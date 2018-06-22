574c1ae060291dd0e1d0059b59b9bcd7ce25a02c
7gh-w <39625441+7gh-w@users.noreply.github.com>
2018-06-22 14:37:33 +0200

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -10,7 +10,7 @@ FRAMERATE = 30
 G = 6.674e-20
 TIME_STEP = 3600 * 24
 V_SIZE = V_WIDTH, V_HEIGHT = 3.5e9, 3.5e9  # Total size of System = 3e9km
-SPEED_FACTORS = [1, 1e3, 1e4, 1e5, 1e6, 5e6, 1e7,
+SPEED_FACTORS = [1, 1e3, 1e4, 1e5, 1.5e5, 3e5, 5e5, 7e5, 1e6, 5e6, 1e7,
                  2e7, 5e7, 1e8]
 SPEED_INDEX = 1
 ZOOM_FACTOR = 1.0
@@ -23,11 +23,13 @@ STARTPOS = [v_center,                             # Sun
             v_center + np.array([0, 24.99e7]),    # Mars
             v_center + np.array([0, 81.9e7]),     # Jupyter
             v_center + np.array([0, 15.1857e8])]  # Saturn
+            
 STARTVEL = [np.array([0, 0]),      # Sun
             np.array([29.29, 0]),  # Earth 29.29
             np.array([21.97, 0]),  # Mars
             np.array([12.45, 0]),  # Jupyter
             np.array([9.11, 0])]   # Saturn
+           
 MASS = [2e30, 5.974e24, 6.419e23, 1.9e27, 5.685e26]
 OBJECT_IMG = [p.image.load('assets/sun.png'),
               p.image.load('assets/earth.png'),
@@ -39,9 +41,10 @@ COLOR = [(255, 255, 0),    # Sun = Yellow
          (255, 0, 0),      # Mars = Red
          (153, 102, 51),   # Jupyter = Brown
          (140, 140, 140)]  # Saturn = Grey
+            
 RADIUS = [695700.0, 6378.0, 3396.0, 69911.0, 60268.0]
-TRACE_LENGTH = [100, 50, 50, 150, 150]
-TRACE_TIME = [24*3600*50, 24*3600*365, 24*3600*687, 24*3600*365*11.8, 24*3600*365*29.5]
+TRACE_LENGTH = [150, 50, 50, 150, 150]
+TRACE_TIME = [ 24*3600*365*11.8, 24*3600*365, 24*3600*687, 24*3600*365*11.8, 24*3600*365*29.5]
 
 # COLORS
 c_i = p.Color('lightskyblue3')
