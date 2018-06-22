6efe491864a9077845164d380fef3b39a136e724
7gh-w <39625441+7gh-w@users.noreply.github.com>
2018-06-22 12:52:53 +0200

    Fix write_input bug, change some variables

--- code/menu.py
+++ code/menu.py
@@ -12,24 +12,6 @@ mediumText = pygame.font.Font('freesansbold.ttf', 43)
 largeText = pygame.font.Font('freesansbold.ttf', 50)
 FONT = pygame.font.Font(None, 32)
 
-# PYGAME FARBEN
-c_i = pygame.Color('lightskyblue3')
-c_a = pygame.Color('dodgerblue2')
-black = (0, 0, 0)
-grey = (50, 131, 134)
-white = (255, 255, 255)
-white_cyan = (255, 250, 240)
-red = (200, 0, 0)
-green = (0, 200, 0)
-yellow = (255, 193, 37)
-bright_yellow = (255, 215, 0)
-bright_red = (255, 0, 0)
-bright_green = (0, 255, 0)
-yellow_launch = (245, 222, 179)
-bright_yellow_launch = (255, 235, 205)
-lime = (0, 255, 0)
-grey = (30, 30, 30)
-
 # BILDER
 MainMenuimg = pygame.image.load('assets/MainMenu.jpg')
 settingimg = pygame.image.load('assets/backgroundblack.jpg')
@@ -75,7 +57,14 @@ def button(message, x, y, w, h, ic, ac, action=None):  # x,y = coord. w=width, h
                 msonde_t1 = msonde_t0 - 2286 * 10**3  # After 150sec
                 msonde_t2 = msonde_t1 - 464 * 10**3   # After 360sec
                 msonde_t3 = msonde_t2 - 114 * 10**3   # After 500sec   source(nasa.wikibis.com)
-              
+                s.MASS[0] = 2e30
+                s.MASS[1] = 5.974e24
+                s.MASS[2] = 6.419e23
+                s.MASS[3] = 1.9e27
+                s.MASS[4] = 5.685e26
+                s.G = 6.674e-20
+                s.TIME_STEP = 3600 * 24
+                
                 parameter_loop()
      # if the mouse isn't in a rectangle, the color will change
     else:
@@ -107,8 +96,8 @@ class InputBox:
 
     def __init__(self, x, y, w, h, text=''):
         self.rect = p.Rect(x, y, w, h)
-        self.color = c_i
         self.text = text  # text wich is inputed
+        self.color = s.c_i
         self.txt_surface = FONT.render(text, True, self.color)
         self.active = False
 
@@ -122,7 +111,7 @@ class InputBox:
             else:
                 self.active = False
             # Change the current color of the input box.
-            self.color = c_a if self.active else c_a
+            self.color = s.c_a if self.active else s.c_a
             # Action that occur if a key is pressed
         if event.type == p.KEYDOWN:
             if self.active:
@@ -137,43 +126,45 @@ class InputBox:
                     s.spaceship = float(self.text)
                     print(s.ss_acc)"""
                     
-                if event.key == p.K_RETURN and 450 + 140 > mouse[0] > 450 and 220 + 32 > mouse[1] > 220:
-                    self.color = lime
+               if event.key == p.K_RETURN and 450 + 300 > mouse[0] > 450 and 220 + 32 > mouse[1] > 220:
+                    self.color = s.lime
                     s.MASS[0] = float(self.text)
                     print(s.MASS[0])
+                    pygame.time.wait(100)
                     
-                elif event.key == p.K_RETURN and 450 + 140 > mouse[0] > 450 and 280 + 32 > mouse[1] > 280:
-                    self.color = lime
+                   
+                elif event.key == p.K_RETURN and 450 + 300 > mouse[0] > 450 and 280 + 32 > mouse[1] > 280:
+                    self.color = s.lime
                     s.MASS[1] = float(self.text)
                     print(s.MASS[1])
 
-                elif event.key == p.K_RETURN and 450 + 140 > mouse[0] > 450 and 340 + 32 > mouse[1] > 340:
-                    self.color = lime
+                elif event.key == p.K_RETURN and 450 + 300 > mouse[0] > 450 and 340 + 32 > mouse[1] > 340:
+                    self.color = s.lime
                     s.G = float(self.text)
                     print(s.G)
 
-                elif event.key == p.K_RETURN and 450 + 140 > mouse[0] > 450 and 400 + 32 > mouse[1] > 400:
-                    self.color = lime
+                elif event.key == p.K_RETURN and 450 + 300 > mouse[0] > 450 and 400 + 32 > mouse[1] > 400:
+                    self.color = s.lime
                     s.TIME_STEP = float(self.text)
                     print(s.TIME_STEP)
-                    
-                elif event.key == p.K_RETURN and 450 + 140 > mouse[0] > 450 and 460 + 32 > mouse[1] > 460:
-                    self.color = lime
+            
+                elif event.key == p.K_RETURN and 450 + 300 > mouse[0] > 450 and 460 + 32 > mouse[1] > 460:
+                    self.color = s.lime
                     s.MASS[2] = float(self.text)
                     print(s.MASS[2])
 
-                elif event.key == p.K_RETURN and 450 + 140 > mouse[0] > 450 and 520 + 32 > mouse[1] > 520:
-                    self.color = lime
+                elif event.key == p.K_RETURN and 450 + 300 > mouse[0] > 450 and 520 + 32 > mouse[1] > 520:
+                    self.color = s.lime
                     s.MASS[3] = float(self.text)
                     print(s.MASS[3])
                     
-                elif event.key == p.K_RETURN and 450 + 140 > mouse[0] > 450 and 580 + 32 > mouse[1] > 580:
-                    self.color = lime
+                elif event.key == p.K_RETURN and 450 + 300 > mouse[0] > 450 and 580 + 32 > mouse[1] > 580:
+                    self.color = s.lime
                     s.MASS[4] = float(self.text)
                     print(s.MASS[4])
                     
                 elif event.key == p.K_BACKSPACE:
-                    self.text = self.text[:-1]
+                    main()
                 elif event.key == p.K_COMMA:
                     self.text = self.text
                 else:
@@ -199,40 +190,42 @@ class InputBox:
 #create InputsBoxes and possibility to change some variables
 def main():
 
-    input_box1 = InputBox(450, 100, 140, 32)
-    input_box2 = InputBox(450, 160, 140, 32)
-    input_box3 = InputBox(450, 220, 140, 32)
-    input_box4 = InputBox(450, 280, 140, 32)
-    input_box5 = InputBox(450, 340, 140, 32)
-    input_box6 = InputBox(450, 400, 140, 32)
-    input_box7 = InputBox(450, 460, 140, 32)
-    input_box8 = InputBox(450, 520, 140, 32)
-    input_box9 = InputBox(450, 580, 140, 32)
-    input_boxes = [input_box1, input_box2, input_box3, input_box4, input_box5, input_box6,input_box7,input_box8, input_box9]
+    i_b0 = InputBox(450, 100, 140, 32)
+    i_b1 = InputBox(450, 160, 140, 32)
+    i_b2 = InputBox(450, 220, 140, 32)
+    i_b3 = InputBox(450, 280, 140, 32)
+    i_b4 = InputBox(450, 340, 140, 32)
+    i_b5 = InputBox(450, 400, 140, 32)
+    i_b6 = InputBox(450, 460, 140, 32)
+    i_b7 = InputBox(450, 520, 140, 32)
+    i_b8 = InputBox(450, 580, 140, 32)
+    i_b = [i_b0, i_b1, i_b2, i_b3, i_b4, i_b5,i_b6,i_b7, i_b8]
 
-    enter = False
     screen.blit(settingimg, (0, 0))
     message_screen('Parameters',mediumText, s.WIDTH/2, 30)
+    message_screen('After input press "Enter". Note that acceleration and mass of spaceship are invalid for yet',Note, 395, 64)
+    message_screen('Valide inputs: 12 or 12.04 or 12e30, calculus are not supported',Note, 280, 81)
     message_screen('Mass of Space Ship',smallText, 320, 115)
     message_screen('Acceleration of Space Ship',smallText, 270, 175)
     message_screen('Mass of Sun',smallText, 360, 235)
     message_screen('Mass of Earth',smallText, 350, 295)
-    message_screen('Delta T [s]',smallText, 370, 355)
+    message_screen('Constant of Gravitation',smallText, 290, 355)
     message_screen('Calculus Steps',smallText, 340, 415)
     message_screen('Mass of Mars',smallText, 340, 475)
     message_screen('Mass of Jupyter',smallText, 340, 535)
     message_screen('Mass of Saturn',smallText, 340, 595)
 
+    enter = False
     while not enter:
         for event in p.event.get():
             if event.type == p.QUIT or event.type == p.KEYDOWN and event.key == p.K_ESCAPE:
                 enter = True
-            for box in input_boxes:
+            for box in i_b:
                 box.write_input(event)
-        for box in input_boxes:
+        for box in i_b:
             box.update()
 
-        for box in input_boxes:
+        for box in i_b:
             box.draw(screen)
 
         p.display.flip()
