bafa9ef48d7e5c2faf45a800f874e7c30bcc1dcc
7gh-w <39625441+7gh-w@users.noreply.github.com>
2018-06-29 14:18:02 +0200

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -7,7 +7,7 @@ SIZE = WIDTH, HEIGHT = 800, 800
 FRAMERATE = 30
 
 # GENERAL SIMULATION
-G = 6.674e-20
+G = 6.6742e-20
 TIME_STEP = 3600 * 24
 V_SIZE = V_WIDTH, V_HEIGHT = 3.5e9, 3.5e9  # Total size of System = 3e9km
 SPEED_FACTORS = [1, 1e3, 1e4, 1e5, 1.5e5, 3e5, 5e5, 7e5, 1e6, 5e6, 1e7,
@@ -20,19 +20,22 @@ v_center = np.array([V_WIDTH/2, V_HEIGHT/2])
 # Space_object Constants
 STARTPOS = [v_center,                             # Sun
             v_center + np.array([0, 15.21e7]),    # Earth
+            v_center + np.array([0, 15.24844e7]), # Moon of Earth
             v_center + np.array([0, 24.99e7]),    # Mars
             v_center + np.array([0, 81.9e7]),     # Jupyter
             v_center + np.array([0, 15.1857e8])]  # Saturn
-
+            
 STARTVEL = [np.array([0, 0]),      # Sun
-            np.array([29.29, 0]),  # Earth 29.29
+            np.array([29.29, 0]),  # Earth
+            np.array([30.26, 0]),  # Moon of Earth
             np.array([21.97, 0]),  # Mars
             np.array([12.45, 0]),  # Jupyter
             np.array([9.11, 0])]   # Saturn
-
-MASS = [2e30, 5.974e24, 6.419e23, 1.9e27, 5.685e26]
+           
+MASS = [2e30, 5.974e24,  7,342e22, 6.419e23, 1.9e27, 5.685e26]
 OBJECT_IMG = [p.image.load('assets/sun.png'),
               p.image.load('assets/earth.png'),
+              p.image.load('assets/moon.png'),
               p.image.load('assets/mars.png'),
               p.image.load('assets/jupiter.png'),
               p.image.load('assets/saturn.png')]
@@ -41,17 +44,16 @@ COLOR = [(255, 255, 0),    # Sun = Yellow
          (255, 0, 0),      # Mars = Red
          (153, 102, 51),   # Jupyter = Brown
          (140, 140, 140)]  # Saturn = Grey
-NAME = ["Sonne", "Erde", "Mars", "Jupiter", "Saturn"]
-
-RADIUS = [695700.0, 6378.0, 3396.0, 69911.0, 60268.0]
-TRACE_LENGTH = [100, 50, 50, 150, 150]
-TRACE_TIME = [24*3600*365*5, 24*3600*365, 24*3600*687, 24*3600*365*11.8, 24*3600*365*29.5]
+            
+RADIUS = [695700.0, 6378.0, 1737.0, 3396.0, 69911.0, 60268.0]
+TRACE_LENGTH = [100, 50, 10, 50, 150, 150]
+TRACE_TIME = [ 24*3600*365*5, 24*3600*365,24*3600*182, 24*3600*687, 24*3600*365*11.8, 24*3600*365*29.5]
 
 # COLORS
 c_i = p.Color('lightskyblue3')
 c_a = p.Color('dodgerblue2')
 black = (0, 0, 0)
-grey = (50, 131, 134)
+grey_b = (50, 131, 134)
 white = (255, 255, 255)
 white_cyan = (255, 250, 240)
 red = (200, 0, 0)
